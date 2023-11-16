2e1ecc0d63a6a36d0d4653d53227466631cc68f1
Daniele <guest.nucera.d2@igt.com>
2023-11-16 14:45:39 +0100

    Nuovo file di test

---
+++ src/test/java/Test2.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test2
+{    @Test
+    public void test2()
+    {
+        System.out.println("Eseguo il test2");
+        Assert.assertTrue("false == false!",false == false);
+    }
+}
