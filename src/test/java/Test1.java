55e6200c66b6e200cbf4c079697c82b864281bad
dannucera <guest.nucera.d2@igt.com>
2023-11-16 13:56:11 +0100

    first commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test1
+{
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Eseguo il test1");
+        Assert.assertTrue("True == false",true != false);
+    }
+}
